6cbc42026d3e32154fbd13bcf5b998b42a826361
profAndreaFranca <dejandreafranca@gmail.com>
2023-11-20 20:56:18 -0300

    Arquivos iniciais, pastas e telas

---
+++ screens/search.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+
+export default class Search extends React.Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>
+                    Search
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#5654d9"
+    }
+})
---
+++ screens/transaction.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+
+export default class Transaction extends React.Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>
+                    Transaction
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"#5654d9"
+    }
+})
