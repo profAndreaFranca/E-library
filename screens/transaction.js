98a37b92b897767f7177b8ce264bb39d1c86dc43
profAndreaFranca <dejandreafranca@gmail.com>
2023-11-22 20:54:19 -0300

    Criado GetCameraPermissions

--- screens/transaction.js
+++ screens/transaction.js
@@ -1,14 +1,40 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import * as Permissions from "expo-permissions"
 
 
 export default class Transaction extends React.Component{
+    constructor(){
+        super()
+        this.state ={
+            domState: "normal", //estado de modo
+            hasCameraPermissions: null, //verifica se tem permissão para usar a camera
+            scanned:false, //ja fez o scanner ou não
+            scannerData:"", //receber o dado escaneado
+        }
+    }
+
+    getCameraPermission = async (donState) => {
+        const {status} = await Permissions.askAsync(Permissions.CAMERA)
+
+        this.setState({
+            hasCameraPermissions: status == "granted",
+            donState:donState,
+            scanned:false
+        })
+    }
     render(){
         return(
             <View style={styles.container}>
                 <Text style={{fontSize:20}}>
                     Transaction
                 </Text>
+                <TouchableOpacity
+                    onPress={()=> this.getCameraPermission("scanner")}
+                >
+                    <Text>Digitalizar QRCode</Text>
+                </TouchableOpacity>
             </View>
         )
     }
