c6e698a7697c17bf391d94b2aa2f32c273a66c67
Profª Andréa França <dejandreafranca@gmail.com>
2023-11-29 20:45:31 -0300

    Update transaction.js

--- screens/transaction.js
+++ screens/transaction.js
@@ -1,86 +1,101 @@
 import React from "react";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import * as Permissions from "expo-permissions"
+import * as Permissions from "expo-permissions";
 
+export default class Transaction extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      domState: "normal", //estado de modo
+      hasCameraPermissions: null, //verifica se tem permissão para usar a camera
+      scanned: false, //ja fez o scanner ou não
+      scannerData: "", //receber o dado escaneado
+    };
+  }
 
-export default class Transaction extends React.Component{
-    constructor(){
-        super()
-        this.state ={
-            domState: "normal", //estado de modo
-            hasCameraPermissions: null, //verifica se tem permissão para usar a camera
-            scanned:false, //ja fez o scanner ou não
-            scannerData:"", //receber o dado escaneado
-        }
+  getCameraPermission = async (donState) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    this.setState({
+      hasCameraPermissions: status == "granted",
+      donState: donState,
+      scanned: false,
+    });
+  };
+  handlebarCodeScanner = async ({ type, data }) => {
+    this.setState({
+      scannerData: data,
+      donState: "normal",
+      scanned: true,
+    });
+  };
+  render() {
+    const { domState, hasCameraPermissions, scannerData, scanned } = this.state;
+    if (domState == "scanner") {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handlebarCodeScanner}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
     }
-
-    getCameraPermission = async (donState) => {
-        const {status} = await Permissions.askAsync(Permissions.CAMERA)
-
-        this.setState({
-            hasCameraPermissions: status == "granted",
-            donState:donState,
-            scanned:false
-        })
-    }
-    handlebarCodeScanner = async ({type,data}) => {
-
-        this.setState({
-            scannerData: data,
-            donState: 'normal',
-            scanned: true,
-        })
-
-    }
-    render(){
-        const{
-            domState,
-            hasCameraPermissions,
-            scannerData,
-            scanned,
-        }= this.state
-        if(domState == "scanner"){
-            return(
-                <BarCodeScanner
-                onBarCodeScanned = {scanned?undefined: this.handlebarCodeScanner}
-                style = {StyleSheet.absoluteFillObject}
-                />
-            )
-        }
-        return(
-            <View style={styles.container}>
-                <Text style={styles.buttomText}>
-                    {hasCameraPermissions? scannerData: "Solicitar permissão da camera"}
-                </Text>
-                <TouchableOpacity
-                style={styles.buttom}
-                    onPress={()=> this.getCameraPermission("scanner")}
-                >
-                    <Text style={styles.buttomText}>Digitalizar QRCode</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.buttomText}>
+          {hasCameraPermissions ? scannerData : "Solicitar permissão da camera"}
+        </Text>
+        <TouchableOpacity
+          style={styles.buttom}
+          onPress={() => this.getCameraPermission("scanner")}
+        >
+          <Text style={styles.buttomText}>Digitalizar QRCode</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:"#5654d9"
+    container: {
+      flex: 1,
+      backgroundColor: "#FFFFFF"
     },
-    buttom:{
-        width:'43%',
-        height:55,
-        justifyContent:"center",
-        alignItems: "center",
-        backgroundColor: "#f48d20", 
-        borderRadius:15
+    lowerContainer: {
+      flex: 0.5,
+      alignItems: "center"
     },
-    buttomText:{
-        fontSize:15,
-        color:"#fff"
+    textinputContainer: {
+      borderWidth: 2,
+      borderRadius: 10,
+      flexDirection: "row",
+      backgroundColor: "#9DFD24",
+      borderColor: "#FFFFFF"
+    },
+    textinput: {
+      width: "57%",
+      height: 50,
+      padding: 10,
+      borderColor: "#FFFFFF",
+      borderRadius: 10,
+      borderWidth: 3,
+      fontSize: 18,
+      backgroundColor: "#5653D4",
+      color: "#FFFFFF"
+    },
+    scanbutton: {
+      width: 100,
+      height: 50,
+      backgroundColor: "#9DFD24",
+      borderTopRightRadius: 10,
+      borderBottomRightRadius: 10,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    scanbuttonText: {
+      fontSize: 24,
+      color: "#0A0101",
     }
-})
+   
+     
+   });
