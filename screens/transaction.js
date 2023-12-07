5445c550813e0ecf0441200c3dbe25a302d908d1
Profª Andréa França <dejandreafranca@gmail.com>
2023-12-07 19:56:32 -0300

    ajustes

--- screens/transaction.js
+++ screens/transaction.js
@@ -6,11 +6,13 @@ import {
   TouchableOpacity,
   TextInput,
   ImageBackground,
+  Alert,
 } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import * as Permissions from "expo-permissions";
 import { Image } from "react-native";
-import db from "../config"
+import db from "../config";
+import firebase from "firebase";
 
 const bgImage = require("../assets/background2.png");
 const appIcon = require("../assets/appIcon.png");
@@ -26,6 +28,8 @@ export default class Transaction extends React.Component {
       scannerData: "", //receber o dado escaneado
       bookId: "",
       studentId: "",
+      bookName: "",
+      studentName: "",
     };
   }
 
@@ -56,11 +60,116 @@ export default class Transaction extends React.Component {
     }
   };
 
-  handleTransaction = ()=>{ //gerenciar as transações
+  //Iago - 4
+  handleTransaction = async () => {
+    //gerenciar as transações
+    var { bookId, studentId } = this.state;
+    await this.getBookDatails(bookId);
+    await this.getStudentDatails(studentId);
 
-  }
+    var { studentName, bookName } = this.state;
 
+    db.collection("books")
+      .doc(bookId)
+      .get()
+      .then((doc) => {
+        var book = doc.data();
+        if (book.is_book_available) {
+          this.initiateBookIssue(bookId,studentId,bookName,studentName);
+          Alert.alert("Voce retirou o livro com sucesso!")
+        } else {
+          this.initiateBookReturn(bookId,studentId,bookName,studentName);
+          Alert.alert("O livro foi devolvido com sucesso!");
+        }
+      });
+  };
 
+  getBookDatails = (bookId) => {
+    bookId = bookId.trim().toLowerCase();
+    db.collection("books")
+      .where("book_id", "==", bookId)
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.map((doc) => {
+          this.setState({ bookName: doc.data().book_name });
+        });
+      });
+  };
+
+  getStudentDatails = (studentId) => {
+    studentId = studentId.trim().toLowerCase();
+    db.collection("students")
+      .where("student_id", "==", studentId)
+      .get()
+      .then((snapshot) => {
+        snapshot.docs.map((doc) => {
+          this.setState({ studentName: doc.data().student_name });
+        });
+      });
+  };
+
+  //criar função para retirada - Francesco - 1
+  initiateBookIssue = (bookId, studentId, bookName, studentName) => {
+    //adicionar nova transação
+    db.collection("transactions").add({
+      student_id: studentId,
+      student_name: studentName,
+      book_id: bookId,
+      book_name: bookName,
+      date: firebase.firestore.Timestamp.now().toDate(),
+      transaction_type: "issue",
+    });
+
+    //alterar o status do livro
+    db.collection("books").doc(bookId).update({
+      is_book_available: false,
+    });
+
+    //alterar quantidade de livos no aluno
+    db.collection("students")
+      .doc(studentId)
+      .update({
+        number_of_books_issued: firebase.firestore.FieldValue.increment(1),
+      });
+
+    //atualizar os estados para vazio
+    this.setState({
+      bookId: "",
+      studentId: "",
+    });
+  };
+
+  //criar função para devolução - Alexandre - 2
+
+  initiateBookReturn = (bookId, studentId, bookName, studentName) => {
+    //adicionar nova transação
+    db.collection("transactions").add({
+      student_id: studentId,
+      student_name: studentName,
+      book_id: bookId,
+      book_name: bookName,
+      date: firebase.firestore.Timestamp.now().toDate(),
+      transaction_type: "return",
+    });
+
+    //alterar o status do livro
+    db.collection("books").doc(bookId).update({
+      is_book_available: true,
+    });
+
+    //alterar quantidade de livos no aluno
+    db.collection("students")
+      .doc(studentId)
+      .update({
+        number_of_books_issued: firebase.firestore.FieldValue.increment(-1),
+      });
+
+    //João Pedro editar os textImput - 3
+    this.setState({
+      bookId: "",
+      studentId: "",
+    });
+  };
   render() {
     const {
       domState,
@@ -82,29 +191,20 @@ export default class Transaction extends React.Component {
       <View style={styles.container}>
         <ImageBackground style={styles.bgImage} source={bgImage}>
           <View style={styles.upperContainer}>
-            <Image source={appIcon} style = {styles.appIcon}/>
-            <Image source={appName} style = {styles.appName}/>
+            <Image source={appIcon} style={styles.appIcon} />
+            <Image source={appName} style={styles.appName} />
           </View>
           <View style={styles.lowerContainer}>
             <View style={styles.textInputContainer}>
               <TextInput
                 style={styles.textInput}
-                placeholder={"id do aluno"}
+                placeholder={"id do Livro"}
                 placeholderTextColor={"#fff"}
-                value={studentId}
-              />
-              <TouchableOpacity
-                style={styles.scanButton}
-                onPress={() => this.getCameraPermission("studentId")}
-              >
-                <Text style={styles.scanButtonText}>scan</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.textInputContainer}>
-              <TextInput
-                style={styles.textInput}
-                placeholder={"id do livro"}
-                placeholderTextColor={"#fff"}
+                onChangeText={(text) =>
+                  this.setState({
+                    bookId: text,
+                  })
+                }
                 value={bookId}
               />
               <TouchableOpacity
@@ -114,9 +214,28 @@ export default class Transaction extends React.Component {
                 <Text style={styles.scanButtonText}>scan</Text>
               </TouchableOpacity>
             </View>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                style={styles.textInput}
+                placeholder={"id do Aluno"}
+                placeholderTextColor={"#fff"}
+                onChangeText={(text) =>
+                  this.setState({
+                    studentId: text,
+                  })
+                }
+                value={studentId}
+              />
+              <TouchableOpacity
+                style={styles.scanButton}
+                onPress={() => this.getCameraPermission("studentId")}
+              >
+                <Text style={styles.scanButtonText}>scan</Text>
+              </TouchableOpacity>
+            </View>
             <TouchableOpacity
-              style = {[styles.button,{marginTop:25}]}
-              onPress = {()=> this.handleTransaction()}
+              style={[styles.button, { marginTop: 25 }]}
+              onPress={() => this.handleTransaction()}
             >
               <Text style={styles.buttonText}>Enviar</Text>
             </TouchableOpacity>
@@ -193,11 +312,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F48D20",
-    borderRadius: 15
+    borderRadius: 15,
   },
   buttonText: {
     fontSize: 24,
     color: "#FFFFFF",
     // fontFamily: "Rajdhani_600SemiBold"
-  }
+  },
 });
