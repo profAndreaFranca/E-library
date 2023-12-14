51a582567fa111f1cad5d185b5416ecde9cd9f65
profAndreaFranca <dejandreafranca@gmail.com>
2023-12-13 21:51:48 -0300

    ajustes tab

--- navigation/tabNavigator.js
+++ navigation/tabNavigator.js
@@ -9,48 +9,41 @@ export default class TabNavigator extends React.Component {
   render() {
     return (
       <Tab.Navigator
-        screenOptions={({ route }) => {
+        screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             var iconName;
             if (route.name == "transaction") {
-              iconName = "book";
+              iconName = focused
+              ? 'book'
+              : 'book-outline';
             } else if (route.name == "search") {
-              iconName = "search";
+              iconName = focused
+              ? 'search'
+              : 'search-outline'
             }
-            return(
-                <Ionicons
-                    name = {iconName}
-                    size = {size}
-                    color = {color}
-                    
-                />
-            )
-          };
-        }}
-        tabBarOptions = {{
-            activeTintColor: '#fff',
-            inactiveTintColor: '#000',
-            style: {
-                height: 130,
-                borderTopWidth: 0,
-                backgroundColor: '#5653d4'
-            },
-            labelStyle: {
-            fontSize: 20
-        },
-        labelPosition:'beside-icon',
-        tabStyle: {
-            marginTop: 25,
-            marginLeft: 10,
-            marginRight: 10,
-            borderRadius: 30,
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#fff",
+          tabBarInactiveTintColor:  "#000",
+          tabBarStyle:{
+            height: 80,
+            // borderTopWidth: 2,
+            backgroundColor: "#5653d4",
+            marginTop: 2,
+            marginLeft: 2,
+            marginRight: 2,
+            borderRadius: 20,
             borderWidth: 2,
             alignItems: "center",
             justifyContent: "center",
-            backgroundColor: "#5653d4"
-        }
-        }}
-
+            // backgroundColor: "#5653d4",
+          },
+          tabBarLabelStyle:{
+            fontSize: 20,
+          },
+          tabBarLabelPosition:"beside-icon",
+          headerShown:false
+        })}
       >
         <Tab.Screen name={"transaction"} component={Transaction} />
         <Tab.Screen name={"search"} component={Search} />
