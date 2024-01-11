5e98f38b3369b5a7c99f47a30561d0e4c4278957
profAndreaFranca <dejandreafranca@gmail.com>
2024-01-10 21:29:53 -0300

    Autenticação do usuário
    Co-authored-by: AlexandreGamboa <AlexandreGamboa@users.noreply.github.com>
    Co-authored-by: Naoeofrancesco <Naoeofrancesco@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from "./navigation/tabNavigator"
+import Login from './screens/loginScreen';
+// import {createSwitchNavigator, createAppContainer} from "react-navigation"
+import { createStackNavigator } from '@react-navigation/stack';
+
+
 
 
 export default function App() {
+  
   return (
    <NavigationContainer>
-    <TabNavigator />
+    <MyStack />
    </NavigationContainer>
   );
 }
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="BottomTab" component={TabNavigator} />
+    </Stack.Navigator>
+  );
+}
+// const AppSwitchNavigator = createSwitchNavigator({
+//   Login: { screen: Login},
+//   BottomTab: { screen: TabNavigator}
+// },{
+//   initialRouteName: "Login"
+// })
+
+// const AppContainer = createAppContainer(AppSwitchNavigator)
